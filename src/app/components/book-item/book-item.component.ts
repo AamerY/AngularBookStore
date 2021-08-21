dee29f9d05cfe5ba97b4203310bfd3fb41a5922f
AamerY <negmaa@gmail.com>
2021-08-21 12:15:18 +0300

    show detauls on one click

--- src/app/components/book-item/book-item.component.ts
+++ src/app/components/book-item/book-item.component.ts
@@ -1,17 +1,25 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Book } from '../../Book';
-
+import { Subscription } from 'rxjs';
+import { BookService } from '../../services/book.service';
 @Component({
   selector: 'app-book-item',
   templateUrl: './book-item.component.html',
   styleUrls: ['./book-item.component.css'],
 })
 export class BookItemComponent {
+  showdetails: boolean = false;
   @Input() book: Book = { title: '', price: '', stock: false };
   @Output() onDeleteBook: EventEmitter<Book> = new EventEmitter();
   @Output() onToggleStock: EventEmitter<Book> = new EventEmitter();
 
-  constructor() {}
+  subscription: Subscription;
+
+  constructor(private bookService: BookService) {
+    this.subscription = this.bookService
+      .onToggle()
+      .subscribe((value: any) => (this.showdetails = value));
+  }
 
   onDelete(book: Book) {
     if (confirm('Are you sure to delete this item ')) {
@@ -19,7 +27,15 @@ export class BookItemComponent {
     }
   }
 
+  onEdit(book: Book) {}
+
   onToggle(book: Book) {
     this.onToggleStock.emit(book);
+    
+  }
+
+  onBookToggle(book: Book) {
+    
+    this.bookService.toggleBook();
   }
 }
--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable,Subject } from 'rxjs';
 import { Book } from '../Book';
+
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -15,6 +16,8 @@ const httpOptions = {
 export class BookService {
   //tested with json server
   private apiUrl = 'http://localhost:3000/books';
+  private showDetails: boolean = false;
+  private subject = new Subject<any>();
 
   constructor(private http: HttpClient) {}
 
@@ -34,5 +37,15 @@ export class BookService {
 
   addBook(book: Book): Observable<Book> {
     return this.http.post<Book>(this.apiUrl, book, httpOptions);
+  }
+
+  toggleBook(): void {
+    this.showDetails = !this.showDetails;
+    //pass new value of showForm
+    this.subject.next(this.showDetails);
+  }
+
+  onToggle(): Observable<any> {
+    return this.subject.asObservable();
   }
 }
